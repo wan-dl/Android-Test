dc18065636beaaef464552734c9b7ad06bbc74f4
Laohutu <shanxi.1991@gmail.com>
2017-02-08 18:14:23 +0800

    Update

---
+++ Template/conf/appium_config.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import os
+import sys
+import time
+import re
+from appium import webdriver
+
+#Read mobile deviceId
+readDeviceId = list(os.popen('adb devices -l').readlines())
+deviceId = re.findall(r'^\w*\b', readDeviceId[1])[0]
+
+#Read mobile os Version
+deviceAndroidVersion = list(os.popen('adb shell getprop ro.build.version.release').readlines())
+osVersion = re.findall(r'^\w*\b',deviceAndroidVersion[0])[0]
+
+def appium_start():
+    config = {
+        'platformName':'Android',
+        'platformVersion':osVersion,
+        'deviceName':deviceId,
+        'appPackage':"com.eg.android.AlipayGphone",
+        'newCommandTimeout':30,    
+        'automationName': 'Appium',
+        'unicodeKeyboard':True,
+        'resetKeyboard':True}
+
+    return  webdriver.Remote('http://localhost:4723/wd/hub', config)
