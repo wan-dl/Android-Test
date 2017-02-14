e88da10bb3d39e5e08da1d6899c6e50c82df9d90
Laohutu <shanxi.1991@gmail.com>
2017-02-14 15:21:45 +0800

    fix

--- Template/tests/test_through_page.py
+++ Template/tests/test_through_page.py
@@ -6,7 +6,9 @@ import sys
 import time
 import unittest
 from configparser import ConfigParser
+
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
 from appium import webdriver
 
 sys.path.append("..")
@@ -17,7 +19,7 @@ from common.utils import MobileSwipe
 
 # config.ini
 cfg = ConfigParser()
-cfg.read('element.ini')
+cfg.read('../conf/element.ini')
 
 class ThroughPage(unittest.TestCase):
 	
@@ -26,12 +28,11 @@ class ThroughPage(unittest.TestCase):
 		self.driver = appium_config.appium_start()
 
 	def test_through_page(self):
-
+		
+		if self.driver.current_activity != ".activity.MainActivity":
+			self.driver.implicitly_wait(10)
 		el_click_id(self.driver,cfg.get('Home',"EL_HOME"))
 		screenshot(self.driver,"../logs/home.png")
-
-		el_click_xpath(self.driver,cfg.get("Home","EL_EL_MOBILE"))
-		screenshot(self.driver,"../logs/mobile.png")
 
 if __name__ == "__main__":
     unittest.main()
