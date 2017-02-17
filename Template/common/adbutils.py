b6f8a5fba9647e1fa12410f9f66e7634d0a69746
Laohutu <shanxi.1991@gmail.com>
2017-02-17 09:55:39 +0800

    adb extend,easy to use adb command

---
+++ Template/common/adbutils.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+'''
+	Android adb utils.
+	Easy to use adb command or method.
+'''
+
+__author__ = "wdl"
+__copyright__ = "Copyright 2017"
+__version__ = "v1.0.0"
+
+import os
+import sys
+import unittest
+from time import sleep
