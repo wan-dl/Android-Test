0ae31176605b16311d1085e55635e6c1732e28af
sx.work@outlook.com <sx.work@outlook.com>
2017-02-18 15:07:46 +0800

    add verifiy

--- Template/tests/test_through_page.py
+++ Template/tests/test_through_page.py
@@ -13,7 +13,7 @@ from appium import webdriver
 
 sys.path.append("..")
 from conf import appium_config
-from common.utils import el_id_click,el_xpath_click
+from common.utils import el_id_click,el_xpath_click,el_text
 from common.utils import screenshot
 from common.utils import MobileSwipe
 
@@ -22,21 +22,24 @@ cfg = ConfigParser()
 cfg.read('../conf/element.ini')
 
 class ThroughPage(unittest.TestCase):
-	
-	@classmethod
-	def setUpClass(self):
-		self.driver = appium_config.appium_start()
-		self.swipe = MobileSwipe()
+    
+    @classmethod
+    def setUpClass(self):
+        self.driver = appium_config.appium_start()
+        self.swipe = MobileSwipe()
 
-	def test_through_page(self):
-		
-		if self.driver.current_activity != ".activity.MainActivity":
-			self.driver.implicitly_wait(10)
-		el_id_click(self.driver,cfg.get('Home',"EL_HOME"))
-		screenshot(self.driver,"../logs/home.png")
-
-		for n in range(5):
-			self.swipe.swipe_down(self.driver)
+    def test_home_page(self):
+        
+        if self.driver.current_activity != ".activity.MainActivity":
+            self.driver.implicitly_wait(10)
+        el_id_click(self.driver,cfg.get('Home',"EL_HOME"))
+        screenshot(self.driver)
+        
+        while 1:
+            self.swipe.swipe_down(self.driver)
+            if u'推荐' in el_text(self.driver,cfg.get('Home',"EL_TJ")):
+                screenshot(self.driver)
+                break
 
 if __name__ == "__main__":
     unittest.main()
