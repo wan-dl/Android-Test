e6ed4e18499381a83fc8690557c6f6b04ffe929a
sx.work@outlook.com <sx.work@outlook.com>
2017-02-19 17:28:29 +0800

    fix

--- Template/tests/test_through_page.py
+++ Template/tests/test_through_page.py
@@ -22,6 +22,10 @@ cfg = ConfigParser()
 cfg.read("../conf/element.ini")
 
 class ThroughHomePage(unittest.TestCase):
+    """
+    TestCase: Home Page Through.
+    Description: 1.元素点击 2.截图 3.点击左上角返回按钮
+    """
    
     #@classmethod,在此类中只进行一次初始化和清理工作 
     @classmethod
@@ -74,37 +78,88 @@ class ThroughHomePage(unittest.TestCase):
         el_id_click(self.driver,cfg.get("Action","Left"))
  
     def test_recommend_class(self):
-        pass
+        """
+        首页:推荐品类
+        """
+        e1 = cfg.get("Home","El_Category1")
+        e2 = cfg.get("Home","El_Category2")
+        e3 = cfg.get("Home","El_Category3")
+        e4 = cfg.get("Home","El_Category4")
+        recommend_class_list = [e1,e2,e3,e4]
+
+        while 1:
+            self.swipe.swipe_down_half(self.driver)
+            if u"更多品类" in el_text(self.driver,cfg.get("Home","EL_More_Category")):
+                screenshot(self.driver)
+                break
+        for e in recommend_class_list:
+            el_id_click(self.driver,e)
+            screenshot(self.driver)
+            el_id_click(self.driver,cfg.get("Action","Back"))      
 
     def test_more_category(self):
-        pass
+        """
+        首页:更多品类
+        """ 
+        if u"更多品类" in el_text(self.driver,cfg.get("Home","El_More_Category")):
+            screenshot(self.driver)
+        else:
+            self.swipe.swipe_down_half(self.driver)
+
+        el_id_click(self.driver,cfg.get("Home","El_More_Category"))
+        screenshot(self.driver)
+        el_id_click(self.driver,cfg.get("Action","Finish"))
 
     def test_recommend_brand(self):
-        pass
+        """
+        首页:推荐品牌
+        """
+        e1 = cfg.get("Home","El_Brand1")
+        e2 = cfg.get("Home","El_Brand2")
+        e3 = cfg.get("Home","El_Brand3")
+        e4 = cfg.get("Home","El_Brand4")
+        recommend_class_list = [e1,e2,e3,e4]
+
+        while 1:
+            self.swipe.swipe_down_half(self.driver)
+            if u"更多品牌" in el_text(self.driver,cfg.get("Home","EL_More_Brand")):
+                screenshot(self.driver)
+                break
+        for e in recommend_class_list:
+            el_id_click(self.driver,e)
+            screenshot(self.driver)
+            el_id_click(self.driver,cfg.get("Action","Left"))   
 
     def test_more_brand(self):
-        pass
+        """
+        首页:更多品牌
+        """
+        el_id_click(self.driver,cfg.get("Home","El_More_Category"))
+        screenshot(self.driver)
+        el_id_click(self.driver,cfg.get("Action","Finish"))
+        
 
     def test_recommend(self):
         """
-        推荐
+        首页:推荐
         """
-        while 1:
-            self.swipe.swipe_down(self.driver)
-            if u"推荐" in el_text(self.driver,cfg.get("Home","El_TJ")):
-                screenshot(self.driver)
-                break
+        self.swipe.swipe_down_half(self.driver)
+        el_id_click(self.driver,cfg.get("Home","El_TJ"))
+        screenshot(self.driver)
+
     def test_recommend_light(self):
         """
-        轻奢
+        首页:轻奢
         """
-        pass
+        el_id_click(self.driver,cfg.get("Home","EL_QS"))
+        screenshot(self.driver)
 
     def test_recommend_big(self):
         """
-        大牌
+        首页:大牌
         """
-        pass
+        el_id_click(self.driver,cfg.get("Home","EL_DP"))
+        screenshot(self.driver)
 
     @classmethod
     def tearDownClass(self):
